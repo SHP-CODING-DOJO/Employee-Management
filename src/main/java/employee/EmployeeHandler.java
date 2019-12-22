9350abf34f5b8072766a3ed5dfa60af6ba8c7f56
Eric Li <eric.q.f.li@oocl.com>
2019-12-22 15:58:06 +0800

    [eric][impl]:implement should_return_all_employees_over_the_age_of_18_when_call_older_than_eighteen

--- src/main/java/employee/EmployeeHandler.java
+++ src/main/java/employee/EmployeeHandler.java
@@ -1,4 +1,10 @@
 package employee;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class EmployeeHandler {
+    public List getOlderThanEighteen(List<Employee> employees) {
+        return employees.stream().filter(employee -> employee.getAge() >= 18).collect(Collectors.toList());
+    }
 }
--- src/test/java/employee/EmployeeTest.java
+++ src/test/java/employee/EmployeeTest.java
@@ -1,6 +1,31 @@
 package employee;
 
 
-public class EmployeeTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+public class EmployeeTest {
+    private List<Employee> employees;
+
+    @Before
+    public void beforeTest() {
+        this.employees = Arrays.asList(new Employee("Max", 17), new Employee("Sepp", 18), new Employee("Nina", 15), new Employee("Mike", 51));
+    }
+
+    @Test
+    public void should_return_all_employees_over_the_age_of_18_when_call_older_than_eighteen() {
+        //Given
+
+        //when
+        EmployeeHandler employeeHandler = new EmployeeHandler();
+        List actualResult = employeeHandler.getOlderThanEighteen(this.employees);
+
+        //Then
+        List expectResult = Arrays.asList(this.employees.get(1), this.employees.get(3));
+        Assert.assertEquals(expectResult, actualResult);
+    }
 }
